3b0c7fb7c559d207bb0b3c399f08f4190f9d3700
Daniel Villacres <93736800+danielvill@users.noreply.github.com>
2024-02-12 11:13:59 -0600

    Cambios para editar y eliminar

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ db = dbase()
 app = Flask(__name__)
 app.secret_key = 'fastcar'
 
+
+@app.route('/',methods=['GET','POST'])
+def run():
+    return redirect(url_for('index'))
+
+
+
 #* Ingreso al sistema
 
 @app.route('/index',methods=['GET','POST'])
@@ -53,7 +60,26 @@ def admin():
         
 
 #* Editar y eliminar  administradores
-    
+@app.route('/delete_adm/<string:ad_name>')
+def elitad(ad_name):
+    ad = db['admin']
+    ad.delete_one({'nombre':ad_name})
+    return redirect(url_for('admin'))
+
+@app.route('/edit_ad/<string:ad_name>', methods=['GET', 'POST'])
+def editad(ad_name):
+    ad = db['admin']
+    cedula = request.form['cedula']
+    nombre = request.form['nombre']
+    rol = request.form['rol']
+    email = request.form['email']
+    clave = request.form['clave']
+
+    if cedula and nombre and rol and email and clave:
+        ad.update_one({'nombre':ad_name},{'$set':{'cedula':cedula,'nombre':nombre,'rol':rol,'email':email,'clave':clave}})
+        return redirect(url_for('admin'))
+    else:
+        return render_template("admin/admin.html")
 
 #*Ingreso de cliente
 @app.route('/admin/in_cliente',methods=['GET','POST'])
@@ -79,13 +105,35 @@ def incliente():
 #*Vista de cliente
 @app.route('/admin/cliente',methods=['GET','POST'])
 def cliente():
-    clie = db['cliente']
-    return render_template('/admin/cliente.html',cliente=clie)# Vista para cliente 
+    clie = db['clientes'].find()
+    return render_template('/admin/cliente.html',clientes=clie)# Vista para cliente 
     
 
 
 #*Editar y eliminar cliente
+@app.route('/delete_cli/<string:cli_name>')
+def elitcli(cli_name):
+    cli = db['clientes']
+    cli.delete_one({'nombre':cli_name})
+    return redirect(url_for('admin'))
+
+@app.route('/edit_cli/<string:cli_name>', methods=['GET', 'POST'])
+def editcli(cli_name):
+    cli = db['clientes']
+    nombre = request.form['nombre']
+    telefono = request.form['telefono']
+    direccion = request.form['direccion']
+    provincia = request.form['provincia']
+    canton = request.form['canton']
+    referencia = request.form['referencia']
+    comentario = request.form['comentario']
     
+    if nombre and telefono and direccion and provincia and canton and referencia and comentario:
+        cli.update_one({'nombre':cli_name},{'$set':{'nombre':nombre,'telefono':telefono,'direccion':direccion,'provincia':provincia,'canton':canton,'referencia':referencia,'comentario':comentario}})
+        return redirect(url_for('cliente'))
+    else:
+        return render_template("admin/cliente.html")
+
 
 #*Ingreso de usuarios
 @app.route('/admin/in_usuario',methods=['GET','POST'])
@@ -109,13 +157,32 @@ def inusuario():
 #*Vista de usuarios
 @app.route('/admin/usuarios',methods=['GET','POST'])
 def usuario():
-    usua = db['usuarios']
+    usua = db['usuarios'].find()
     return render_template('/admin/usuarios.html',usuarios=usua)# Vista para usuarios
-
-        
 
 
 #*Editar y eliminar usuarios
+@app.route('/delete_usu/<string:usu_name>')
+def elitusu(usu_name):
+    usu = db['usuarios']
+    usu.delete_one({'nombre':usu_name})
+    return redirect(url_for('usuario'))
+
+@app.route('/edit_usu/<string:usu_name>', methods=['GET', 'POST'])
+def editusu(usu_name):
+    usu = db['usuarios']
+    cedula = request.form['cedula']
+    nombre = request.form['nombre']
+    rol = request.form['rol']
+    email = request.form['email']
+    clave = request.form['clave']
+    
+    if cedula and nombre and rol and email and clave:
+        usu.update_one({'nombre':usu_name},{'$set':{'cedula':cedula,'nombre':nombre,'rol':rol,'email':email,'clave':clave}})
+        return redirect(url_for('usuario'))
+    else:
+        return render_template("admin/usuarios.html")
+    
 
 
 #*Ingreso de conductores
@@ -145,8 +212,35 @@ def inconductores():
 #*Vista de Conductores
 @app.route('/admin/conductores',methods=['GET','POST'])
 def conductores():
-    condu = db['conductores']
+    condu = db['conductores'].find()
     return render_template('/admin/conductores.html',conductores=condu)# Vista para conductores
+
+# * Editar y Eliminar Conductores
+@app.route('/delete_cond/<string:cond_name>')
+def elitcondu(cond_name):
+    cond = db['conductores']
+    cond.delete_one({'nombre':cond_name})
+    return redirect(url_for('conductores'))
+
+@app.route('/edit_cond/<string:cond_name>', methods=['GET', 'POST'])
+def editcondu(cond_name):
+    cond = db['conductores']
+    cedula = request.form['cedula']
+    nombre = request.form['nombre']
+    email = request.form['email']
+    telefono = request.form['telefono']
+    direccion = request.form['direccion']
+    em_nombre = request.form['em_nombre']
+    em_telefono = request.form['em_telefono']
+    em_relacion = request.form['em_relacion']
+    clave = request.form['clave']
+    licencia = request.form['licencia']
+    
+    if cedula and nombre and email and telefono and direccion and em_nombre and em_telefono and em_relacion and clave and licencia:
+        cond.update_one({'nombre':cond_name},{'$set':{'cedula':cedula,'nombre':nombre,'email':email,'telefono':telefono,'direccion':direccion,'em_nombre':em_nombre,'em_telefono':em_telefono,'em_relacion':em_relacion,'clave':clave,'licencia':licencia}})
+        return redirect(url_for('conductores'))
+    else:
+        return render_template("admin/conductores.html")
 
 
 #*Ingreso de unidades
@@ -175,9 +269,35 @@ def inunidades():
 #*Vista de unidades
 @app.route('/admin/unidades',methods=['GET','POST'])
 def unidades():
-    uni = db['unidades']
-    return render_template('/admin/conductores.html',unidades=uni)# Vista de unidades
+    uni = db['unidades'].find()
+    return render_template('/admin/unidades.html',unidades=uni)# Vista de unidades
 
+#*Editar y eliminar unidades
+@app.route('/delete_uni/<string:uni_name>')
+def elituni(uni_name):
+    uni = db['unidades']
+    uni.delete_one({'placa':uni_name})
+    return redirect(url_for('unidades'))
+
+@app.route('/edit_uni/<string:uni_name>', methods=['GET', 'POST'])
+def edituni(uni_name):
+    uni = db['unidades']
+    placa = request.form['placa']
+    modelo = request.form['modelo']
+    marca = request.form['marca']
+    codigo = request.form['codigo']
+    kilometraje = request.form['kilometraje']
+    observacion = request.form['observacion']
+    orden = request.form['orden']
+    es_codigo = request.form['es_codigo']
+    es_tipo = request.form['es_tipo']
+    es_fecha = request.form['es_fecha']
+    
+    if placa and modelo and marca and codigo and kilometraje and observacion and orden and es_codigo and es_tipo and es_fecha:
+        uni.update_one({'placa':uni_name},{'$set':{'placa':placa,'modelo':modelo,'marca':marca,'codigo':codigo,'kilometraje':kilometraje,'observacion':observacion,'orden':orden,'es_codigo':es_codigo,'es_tipo':es_tipo,'es_fecha':es_fecha}})
+        return redirect(url_for('unidades'))
+    else:
+        return render_template("admin/unidades.html")
 
 
 #* Carreras
@@ -195,7 +315,7 @@ def incarreras():
         comentario = request.form['comentario']
         fecha_fin = request.form['fecha_fin']
         estado = request.form['estado']
-        operador = request
+        operador = request.form['operador']
         if cliente and provincia and canton and lugar and referencia and tipo and fecha_inicio and comentario and fecha_fin and estado and operador:
             regis = Carreras(cliente, provincia, canton, lugar, referencia, tipo, fecha_inicio, comentario, fecha_fin, estado, operador)
             carrera.insert_one(regis.CareDBCollection())
@@ -207,8 +327,38 @@ def incarreras():
 #*Vista de carreras
 @app.route('/admin/carreras',methods=['GET','POST'])
 def carreras():
+    carre = db['carreras'].find()
+    return render_template('/admin/carreras.html',carreras=carre)# Vista de carreras
+
+#* Editar y Eliminar Carreras
+
+@app.route('/delete_car/<string:car_name>')
+def elitcarre(car_name):
     carre = db['carreras']
-    return render_template('/admin/conductores.html',carreras=carre)# Vista de carreras
+    carre.delete_one({'cliente':car_name})
+    return redirect(url_for('carreras'))
+
+@app.route('/edit_car/<string:car_name>', methods=['GET', 'POST'])
+def editcarre(car_name):
+    carre = db['carreras']
+    cliente = request.form['cliente']
+    provincia =request.form['provincia']
+    canton = request.form['canton']
+    lugar = request.form['lugar']
+    referencia = request.form['referencia']
+    tipo = request.form['tipo']
+    fecha_inicio = request.form['fecha_inicio']
+    comentario = request.form['comentario']
+    fecha_fin = request.form['fecha_fin']
+    estado = request.form['estado']
+    operador = request.form['operador']
+    
+    if cliente and provincia and canton and lugar and referencia and tipo and fecha_inicio and comentario and fecha_fin and estado and operador:
+        carre.update_one({'cliente':car_name},{'$set':{'cliente':cliente,'provincia':provincia,'canton':canton,'lugar':lugar,'referencia':referencia,'tipo':tipo,'fecha_inicio':fecha_inicio,'comentario':comentario,'fecha_fin':fecha_fin,'estado':estado,'operador':operador}})
+        return redirect(url_for('carreras'))
+    else:
+        return render_template("admin/carreras.html")
+
 
 
 
